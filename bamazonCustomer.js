cc38e956d1a0103e835c396a0a87f29c206a9825
Maryam EKBATANI <maryamekbatani@Maryams-MacBook-Pro.local>
2017-11-10 16:01:21 -0500

    updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,22 +1,19 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3000,
 
   // Your username
   user: "root",
 
   // Your password
   password: "",
-  database: "bamazon_DB"
+  database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
-});
 
 //Functions
 function displayAll() {
@@ -37,7 +34,7 @@ function displayAll() {
                 [response[i].item_id, response[i].product_name, response[i].department_name, response[i].price, response[i].stock_quantity]
             );
         }
-        //log the completed table to console
+        //amazon products should appear in console
         console.log(theDisplayTable.toString());
         inquireForPurchase();
     });
@@ -50,13 +47,13 @@ function displayAll() {
 //////////////////
 
 function inquireForPurchase() {
-    //get item ID and desired quantity from user. Pass to purchase from Database
+    //get item ID and quantity from user. 
     inquirer.prompt([
 
         {
             name: "id",
             type: "input",
-            message: "What is the item number of the item you wish to purchase?"
+            message: "What is the item number of the product you wish to purchase?"
         }, {
             name: 'quantity',
             type: 'input',
@@ -70,12 +67,13 @@ function inquireForPurchase() {
         purchaseFromDatabase(IDDesired, quantityDesired);
     });
 
-}; //end inquireForPurchase
+}; 
+//////////////////////////////
 
 // 
 function purchaseFromDatabase(ID, quantityNeeded) {
-    //check quantity of desired purchase. Minus quantity of the itemID from database if possible. Else inform user "Quantity desired not in stock" 
-    connection.query('SELECT * FROM products WHERE item_id = ' + item_id, function(error, response) {
+    //check to see if product is availabel for purchase. Subtract quantity purchased from stock in data base. If product not available, inform customer.
+    connection.query('SELECT * FROM products WHERE item_id = ' + ID, function(error, response) {
         if (error) { console.log(error) };
 
         //if in stock
@@ -83,97 +81,19 @@ function purchaseFromDatabase(ID, quantityNeeded) {
             //calculate cost
             var totalCost = response[0].price * quantityNeeded;
             //inform user
-            console.log("We have what you need! I'll have your order right out!");
-            console.log("Your total cost for " + quantityNeeded + " " + response[0].product_name + " is " + totalCost + ". Thank you for your Business!");
+            console.log("Item is in stock!");
+            console.log("Your total cost for " + quantityNeeded + " " + response[0].product_name + " is " + totalCost );
             //update database, minus purchased quantity
-            connection.query('UPDATE products SET StockQuantity = StockQuantity - ' + quantityNeeded + ' WHERE item_id = ' + item_id;
+            connection.query('UPDATE products SET stock_quantity = stock_quantity - ' + quantityNeeded + ' WHERE item_id = ' + ID);
         } else {
             console.log("Our apologies. We don't have enough " + response[0].product_name + " to fulfill your order.");
         };
-        displayAll();//recursive shopping is best shopping! Shop till you drop!
+        displayAll();
     });
 
-  //displayAll();
 
-}; //end purchaseFromDatabase
 
-// function multiSearch() {
-//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//   connection.query(query, function(err, res) {
-//     for (var i = 0; i < res.length; i++) {
-//       console.log(res[i].artist);
-//     }
-//     runSearch();
-//   });
-// }
+}; 
 
-// function rangeSearch() {
-//   inquirer
-//     .prompt([
-//       {
-//         name: "start",
-//         type: "input",
-//         message: "Enter starting position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       },
-//       {
-//         name: "end",
-//         type: "input",
-//         message: "Enter ending position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//       connection.query(query, [answer.start, answer.end], function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             "Position: " +
-//               res[i].position +
-//               " || Song: " +
-//               res[i].song +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Year: " +
-//               res[i].year
-//           );
-//         }
-//         runSearch();
-//       });
-//     });
-// }
+  displayAll();
 
-// function songSearch() {
-//   inquirer
-//     .prompt({
-//       name: "song",
-//       type: "input",
-//       message: "What song would you like to look for?"
-//     })
-//     .then(function(answer) {
-//       console.log(answer.song);
-//       connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-//         console.log(
-//           "Position: " +
-//             res[0].position +
-//             " || Song: " +
-//             res[0].song +
-//             " || Artist: " +
-//             res[0].artist +
-//             " || Year: " +
-//             res[0].year
-//         );
-//         runSearch();
-//       });
-//     });
-// }
---
+++ wishes_db.sql
@@ -0,0 +1,1 @@
+SELECT * FROM wishes_db.wishes;
